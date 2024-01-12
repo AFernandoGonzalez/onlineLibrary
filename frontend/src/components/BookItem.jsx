2db3689ea9b3ae8aac5fd6c4716a2460e49a8d93
Fernando <ferg.developer@gmail.com>
2024-01-11 22:13:19 -0500

    added route for getin books details

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BookItem = ({ book }) => (
-    <li key={book.key}>
-        <Link to={`/books/${encodeURIComponent(book.key)}`}>
-            <img
-                src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
-                alt={`Cover for ${book.title}`}
-                style={{ width: '50px', height: '75px' }}
-            />
-            <p>{book.title} by {book.author_name && book.author_name.join(', ')}</p>
-        </Link>
-    </li>
-);
+const BookItem = ({ book }) => {
+    const getKeys = book.key.split('/');
+    console.log(getKeys[2]);
+
+    return (
+        <li key={book.key}>
+            <Link to={`/books/${getKeys[2]}`}>
+                <img
+                    src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+                    alt={`Cover for ${book.title}`}
+                    style={{ width: '50px', height: '75px' }}
+                />
+                <p>{book.title} by {book.author_name && book.author_name.join(', ')}</p>
+            </Link>
+        </li>
+    )
+};
 
 export default BookItem;
